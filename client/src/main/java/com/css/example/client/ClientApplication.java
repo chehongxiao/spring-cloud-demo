10dfd994557567aed874229d81ecd002ce75489d
chehx <306944255@qq.com>
2019-08-20 15:49:09 +0800

    ribbon使用断路器

--- client/src/main/java/com/css/example/client/ClientApplication.java
+++ client/src/main/java/com/css/example/client/ClientApplication.java
@@ -2,8 +2,10 @@ package com.css.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix
 public class ClientApplication {
 
     @Bean
--- client/src/main/java/com/css/example/client/controller/RibbonController.java
+++ client/src/main/java/com/css/example/client/controller/RibbonController.java
@@ -1,6 +1,7 @@
 package com.css.example.client.controller;
 
 import com.css.example.client.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +12,23 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/ribbon")
 public class RibbonController {
-    private static final String REST_URL_PREFIX = "http://cloud-provider";
+    private static final String REST_URL_PREFIX = "http://CLOUD-PROVIDER";
 
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/user/{id}")
+    @HystrixCommand(fallbackMethod = "serviceFailture")
     public User findById(@PathVariable Long id) {
         return restTemplate.getForObject(REST_URL_PREFIX+"/user/"+id,User.class);
+    }
+
+    public User serviceFailture(Long id){
+        User user = new User();
+        user.setId(-1L);
+        user.setUsername("断路器");
+        user.setAge(0);
+        user.setBalance((double) 0);
+        return user;
     }
 }
