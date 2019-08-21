c08c91ba43999aaa002eb587bb9fb094b2196e19
chehx <306944255@qq.com>
2019-08-21 15:07:42 +0800

    增加hystrix dashboad仪表盘

--- client/src/main/java/com/css/example/client/ClientApplication.java
+++ client/src/main/java/com/css/example/client/ClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ClientApplication {
 
     @Bean
